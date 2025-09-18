31f18ad11155e555d7602c22679f029c6190d9b2
AnkitHaldar2006 <haldarankit2006@gmail.com>
2025-09-18 20:53:34 +0530

    Add files via upload

---
+++ python projetcts/Simple calculator/Simple calculator.py
@@ -0,0 +1,12 @@
+def calculator(a, b, op):
+    if op == '+': return a + b
+    elif op == '-': return a - b
+    elif op == '*': return a * b
+    elif op == '/': return a / b
+    else: return "Invalid operator"
+
+print(calculator(10, 5, '+'))  # Output: 15
+print(calculator(10, 5, '-'))  # Output: 5
+print(calculator(10, 5, '*'))  # Output: 50
+print(calculator(10, 5, '/'))  # Output: 2.0
+print(calculator(10, 5, '%'))  # Output: "Invalid operator"
